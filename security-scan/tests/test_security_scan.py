5d35112b8bd003bd14eab1d7a5928e414e1770d6
Joseph Arceneaux <joe.arceneaux@gmail.com>
2019-04-21 17:13:56 -0500

    Start adding tests

---
+++ security-scan/tests/test_security_scan.py
@@ -0,0 +1,24 @@
+"""
+Tests for AWS security scanning
+"""
+
+
+import unittest
+
+
+class TestSecurityScan(unittest.TestCase):
+    """
+    Tests for various security aspects of AWS infrastructure
+    """
+
+    def setUp(self):
+        """
+        Prepare for security scan tests.
+        """
+
+    def test_s3_scan(self):
+        """
+        Test S3 scanning
+        """
+
+        self.assertTrue(True)
